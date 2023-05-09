60f7510fe5b2a7d58004dc931739dda0e5e51675
Lucas Mendonça <lucasmendoncaesilva@gmail.com>
2023-05-08 21:25:06 -0300

    Project created

---
+++ src/test/java/com/lucasilva/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasilva.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
