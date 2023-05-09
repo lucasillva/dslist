52e77aabad442f688294efa16cecbf7beabe7d80
Lucas Mendonça <lucasmendoncaesilva@gmail.com>
2023-05-08 22:35:22 -0300

    Game repository, service, controller

---
+++ src/main/java/com/lucasilva/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.lucasilva.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucasilva.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
